02a057f3865523d7febf127830b1adeca5b09c32
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-21 22:30:24 +0900

    style : 전역 스타일 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
             {/* tanstack query provier */}
             <Providers>
                 <body className={pretendard.className}>
-                    <Suspense fallback={<GlobalLoading />}>{children}</Suspense>
+                    <div className="w-full h-full bg-background">
+                        <Suspense fallback={<GlobalLoading />}>{children}</Suspense>
+                    </div>
                 </body>
             </Providers>
         </html>
