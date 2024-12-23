f9593395627e4062b52b258cddfa96fc82a6782d
cara <cara656513@gmail.com>
2024-12-23 18:26:59 +0900

    style/new-postbox ui, global css title line-height 삭제

--- src/app/new-postbox/page.tsx
+++ src/app/new-postbox/page.tsx
@@ -1,35 +1,67 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import snake from "@/assets/images/snake.png";
+import Button from "@/components/common/Button";
+import ProgressBar from "@/components/common/ProgressBar";
+import { InputForm } from "@/components/common";
 
 const NewPostbox = () => {
     return (
         <div>
-            <div className="title">
-                <div>
+            <ProgressBar />
+            <div className="pt-[53px]">
+                <div className="title">
                     <span className="text-primary">어떤 이름</span>으로
+                    <br />
+                    편지함을 만드시겠어요?
                 </div>
-                <div>편지함을 만드시겠어요?</div>
+                <div className="flex justify-end pt-[24px] pb-[34px]">
+                    <Image width={65.54} height={71} src={snake} alt="snake" />
+                </div>
+                <div className="grid">
+                    <div className="text-[20px] font-semibold pb-[12px]">닉네임</div>
+                    <InputForm
+                        type="text"
+                        full={true}
+                        value=""
+                        handleChange={() => console.log("Input Changed!")}
+                        maxLength={8}
+                    />
+                </div>
+                <div className="text-[16px]">
+                    <div>
+                        <input type="checkbox" />
+                        전체 동의
+                    </div>
+                    <hr />
+                    <div>
+                        <input type="checkbox" />
+                        <span className="text-primary">(필수)</span> 만 14세 이상이에요
+                    </div>
+                    <div>
+                        <input type="checkbox" />
+                        <span className="text-primary">(필수)</span> 이용약관 및 개인정보수집이용 동의
+                    </div>
+                </div>
+                <div className="flex gap-[20px]">
+                    <Button
+                        type="button"
+                        color="btn-white"
+                        full={false}
+                        label="이전"
+                        handleClick={() => alert("Button clicked!")}
+                    />
+                    <Button
+                        type="button"
+                        color="btn-blue"
+                        full
+                        label="다음으로"
+                        handleClick={() => alert("Button clicked!")}
+                    />
+                </div>
             </div>
-            <Image width={65.54} height={71} src="/assets/snake.png" alt="snake" />
-            <div className="grid text-[20px]">
-                닉네임
-                <input type="text" placeholder="0/8" />
-            </div>
-            <div className="text-[16px]">
-                <div>
-                    <input type="checkbox" />
-                    전체 동의
-                </div>
-                <div>
-                    <input type="checkbox" />
-                    (필수) 만 14세 이상이에요
-                </div>
-                <div>
-                    <input type="checkbox" />
-                    (필수) 이용약관 및 개인정보수집이용 동의
-                </div>
-            </div>
-            <button className="bg-[#3B61B9] text-white w-[500px] h-[60px]">다음으로</button>
         </div>
     );
 };
