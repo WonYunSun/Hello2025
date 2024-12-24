6c3d3fbd058d69858cb71243b4db419451245c48
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2024-12-24 10:16:18 +0900

    feat: 마이페이지 그룸핑 페이지 생성

---
+++ src/app/(mypage)/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SettingsPage = () => {
+    return (
+        <div className="inner">
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default SettingsPage;
---
+++ src/app/(mypage)/unregister/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Unregister = () => {
+    return <div>Unregister</div>;
+};
+
+export default Unregister;
