da80709b51acd4f095bf7fbcd60ff6dc34063c47
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-24 17:28:06 +0900

    feat : Letter 컴포넌트 스타일링

--- src/app/decoration/_components/Letter.tsx
+++ src/app/decoration/_components/Letter.tsx
@@ -1,18 +1,22 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import snake from "@/assets/images/snake.svg";
 import { Button } from "@/components/common";
 import { DecorationData } from "./DecorationForms";
+import LetterList from "./LetterList";
 
 type LetterProps = {
+    prevSelectedLetter: string;
     onNext: (data: Pick<DecorationData, "letter">) => void;
     onPrev: () => void;
 };
 
-const Letter = ({ onNext, onPrev }: LetterProps) => {
+const Letter = ({ prevSelectedLetter, onNext, onPrev }: LetterProps) => {
+    const [selectedLetter, setSelectedLetter] = useState<string>(prevSelectedLetter);
+
     const handleNext = () => {
-        onNext({ letter: "letter-envelope" });
+        onNext({ letter: selectedLetter });
     };
 
     return (
@@ -28,7 +32,7 @@ const Letter = ({ onNext, onPrev }: LetterProps) => {
                         <Image src={snake} width={65} height={70} alt="snake" />
                     </div>
                 </header>
-                {/* <EnvelopeList selectedEnvelope={selectedEnvelope} onEnvelopeSelect={setSelectedEnvelope} /> */}
+                <LetterList selectedLetter={selectedLetter} onLetterSelect={setSelectedLetter} />
                 <div className="flex gap-5">
                     <Button type="button" color="btn-white" full={false} label="이전" handleClick={onPrev} />
                     <Button type="button" color="btn-blue" full={true} label="다음으로" handleClick={handleNext} />
