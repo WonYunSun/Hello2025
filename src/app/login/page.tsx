b7f784321f236ef515a6aac394c70b707ff08e98
zi0w <zzz13588@naver.com>
2024-12-24 13:16:45 +0900

    style: 소셜 로그인 버튼 css 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,11 +4,13 @@ import titleImage from "@/assets/images/title.svg";
 import pineTree from "@/assets/images/소나무.svg";
 import deco from "@/assets/images/deco.svg";
 import ground from "@/assets/images/땅.svg";
+import googleLogo from "@/assets/images/구글 로고.svg";
+import kakaoLogo from "@/assets/images/카카오 로고.svg";
 
 const LoginPage = () => {
     return (
         <div className="relative min-h-screen overflow-hidden">
-            <div className="flex flex-col items-center relative mt-24 md:mt-44">
+            <div className="flex flex-col items-center relative mt-24 md:mt-18.3vh">
                 <Image src={titleImage} width={301.15} height={186} alt="title_img" className="w-[60%] md:w-[301px]" />
                 <span className="mt-6 text-[16px] md:text-[20px] text-[#544A40] font-medium">
                     소중한 사람들과 새해 인사를 나눠보세요!
@@ -24,11 +26,17 @@ const LoginPage = () => {
                 </div>
             </div>
             <div className="flex flex-col items-center gap-3 md:gap-4 mt-12 md:mt-24 w-full">
-                <button className="w-4/6 h-10 md:h-12 border text-sm md:text-base bg-white rounded-md">Google 계정으로 시작</button>
-                <button className="w-4/6 h-10 md:h-12 border text-sm md:text-base bg-amber-300 rounded-md">카카오 계정으로 시작</button>
+                <button className="w-4/6 h-10 md:h-12 border text-sm md:text-base bg-[#ffffff] font-medium text-[#666666] border-[#dddddd] rounded-md flex items-center gap-20">
+                    <Image src={googleLogo} width={24} height={24} alt="googleLogo" className="ml-8"/>
+                    <span>Google 계정으로 시작</span>
+                </button>
+                <button className="w-4/6 h-10 md:h-12 border text-sm md:text-base bg-[#FFEB00] font-medium text-[#333333] border-[#dddddd] rounded-md flex items-center gap-20">
+                    <Image src={kakaoLogo} width={24} height={24} alt="kakaoLogo" className="ml-8"/>
+                    <span>카카오 계정으로 시작</span>
+                </button>
             </div>
             <div className="absolute z-20 bottom-10 w-full flex justify-center">
-                <Image src={deco} width={567.47} height={248} alt="deco_img" className="w-[90%] md:w-[567px]"/>
+                <Image src={deco} width={567.47} height={248} alt="deco_img" className="w-[90%] md:w-[567px]" />
             </div>
             <div className="fixed bottom-0 z-10 w-full">
                 <Image src={ground} width={600} height={125} alt="ground_img" />
