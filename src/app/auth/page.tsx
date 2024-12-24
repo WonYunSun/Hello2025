3b927c7be42f71b3bc8630c0f2bf1fb54484282a
zi0w <zzz13588@naver.com>
2024-12-24 20:19:06 +0900

    feat: 구글 로그인 기능 구현

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,8 +4,9 @@ import titleImage from "@/assets/images/title.svg";
 import pineTree from "@/assets/images/소나무.svg";
 import deco from "@/assets/images/deco.svg";
 import ground from "@/assets/images/땅.svg";
-import googleLogo from "@/assets/images/구글 로고.svg";
 import kakaoLogo from "@/assets/images/카카오 로고.svg";
+
+import GoogleLoginBtn from "./_components/GoogleLoginBtn";
 
 const LoginPage = () => {
     return (
@@ -15,7 +16,7 @@ const LoginPage = () => {
                 <span className="mt-6 text-[16px] md:text-[20px] text-[#544A40] font-medium">
                     소중한 사람들과 새해 인사를 나눠보세요!
                 </span>
-                <div className="absolute -bottom-8 md:-bottom-12 left-0 z-10">
+                <picture className="absolute -bottom-8 md:-bottom-12 left-0 z-10">
                     <Image
                         src={pineTree}
                         width={119.2}
@@ -23,24 +24,21 @@ const LoginPage = () => {
                         alt="pine_tree_img"
                         className="w-[80px] md:w-[119px]"
                     />
-                </div>
+                </picture>
             </div>
             <div className="flex flex-col items-center gap-3 md:gap-4 mt-12 md:mt-24 w-full">
-                <button className="w-4/6 h-10 md:h-12 border text-sm md:text-base bg-[#ffffff] font-medium text-[#666666] border-[#dddddd] rounded-md flex items-center gap-20">
-                    <Image src={googleLogo} width={24} height={24} alt="googleLogo" className="ml-8"/>
-                    <span>Google 계정으로 시작</span>
-                </button>
-                <button className="w-4/6 h-10 md:h-12 border text-sm md:text-base bg-[#FFEB00] font-medium text-[#333333] border-[#dddddd] rounded-md flex items-center gap-20">
+                <GoogleLoginBtn />
+                <button className="w-4/6 h-10 md:h-12 border text-sm md:text-base bg-[#FFEB00] font-medium text-[#333333] border-[#dddddd] rounded-md flex items-center gap-8 md:gap-20">
                     <Image src={kakaoLogo} width={24} height={24} alt="kakaoLogo" className="ml-8"/>
                     <span>카카오 계정으로 시작</span>
                 </button>
             </div>
-            <div className="absolute z-20 bottom-10 w-full flex justify-center">
+            <picture className="absolute z-20 bottom-10 w-full flex justify-center">
                 <Image src={deco} width={567.47} height={248} alt="deco_img" className="w-[90%] md:w-[567px]" />
-            </div>
-            <div className="fixed bottom-0 z-10 w-full">
+            </picture>
+            <picture className="fixed bottom-0 z-10 w-full">
                 <Image src={ground} width={600} height={125} alt="ground_img" />
-            </div>
+            </picture>
         </div>
     );
 };
