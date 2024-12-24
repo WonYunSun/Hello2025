3d6dc2a170e2d34873c88f00fae5e19355aa4676
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-24 18:13:14 +0900

    chore : React Fragment 삭제

--- src/app/decoration/_components/Letter.tsx
+++ src/app/decoration/_components/Letter.tsx
@@ -20,25 +20,23 @@ const Letter = ({ prevSelectedLetter, onNext, onPrev }: LetterProps) => {
     };
 
     return (
-        <>
-            <section className="w-full h-full flex flex-col justify-between">
-                <header className="relative">
-                    <h1 className="title">
-                        <span className="text-primary">김철수</span>님 에게 보낼
-                        <br />
-                        편지지를 골라주세요
-                    </h1>
-                    <div className="absolute top-16 right-0">
-                        <Image src={snake} width={65} height={70} alt="snake" />
-                    </div>
-                </header>
-                <LetterList selectedLetter={selectedLetter} onLetterSelect={setSelectedLetter} />
-                <div className="flex gap-5">
-                    <Button type="button" color="btn-white" full={false} label="이전" handleClick={onPrev} />
-                    <Button type="button" color="btn-blue" full={true} label="다음으로" handleClick={handleNext} />
+        <section className="w-full h-full flex flex-col justify-between">
+            <header className="relative">
+                <h1 className="title">
+                    <span className="text-primary">김철수</span>님 에게 보낼
+                    <br />
+                    편지지를 골라주세요
+                </h1>
+                <div className="absolute top-16 right-0">
+                    <Image src={snake} width={65} height={70} alt="snake" />
                 </div>
-            </section>
-        </>
+            </header>
+            <LetterList selectedLetter={selectedLetter} onLetterSelect={setSelectedLetter} />
+            <div className="flex gap-5">
+                <Button type="button" color="btn-white" full={false} label="이전" handleClick={onPrev} />
+                <Button type="button" color="btn-blue" full={true} label="다음으로" handleClick={handleNext} />
+            </div>
+        </section>
     );
 };
 
