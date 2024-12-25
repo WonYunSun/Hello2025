54bd4c315295a9ab9beba3f8ca86201a8e41f167
WonYunSun <ereunosun113@gmail.com>
2024-12-26 01:11:39 +0900

    feat : 유저 별 편지함 페이지 구현, 실제 편지 데이터 표시시

---
+++ src/app/letterbox/[id]/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Button } from "@/components/common";
+import LetterList from "../_components/LetterList";
+import { useEffect, useState } from "react";
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+type LetterType = {
+    id: string;
+    sender_id: string;
+    recipient_id: string;
+    content: string;
+    created_at: string;
+    envelope_type: number;
+    paper_type: number;
+    is_public: boolean;
+};
+type LettersType = {
+    username: string;
+    letters: LetterType[];
+};
+
+const fetchLetters = async (id: string) => {
+    const res = await fetch(`/api/letters?id=${id}`);
+    if (!res.ok) {
+        throw new Error("Failed to fetch letters");
+    }
+    return res.json();
+};
+
+const LetterBox = ({ params }: Props) => {
+    const [letters, setLetters] = useState<LettersType | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState<null | string>(null);
+    useEffect(() => {
+        const getLetters = async () => {
+            try {
+                const data = await fetchLetters(params.id);
+                setLetters(data);
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        getLetters();
+    }, [params.id]);
+
+    if (isLoading) return <div className="inner">Loading...</div>;
+    if (error) return <div className="inner">Error: {error}</div>;
+    if (!letters) return <div className="inner">No letters found.</div>;
+    console.log(letters);
+    return (
+        <div className="inner">
+            <div className="title">
+                <span className="text-primary">{letters.username}</span>님의 편지함입니다.
+                <h3 className="text-[16px] font-semibold">
+                    총 <span className="text-primary font-semibold">{letters.letters.length}</span>개의 편지가
+                    도착했습니다.
+                </h3>
+            </div>
+            {letters.letters.length === 0 ? (
+                <div className="inner">아직 도착한 편지가 없어요.</div>
+            ) : (
+                <LetterList letters={letters.letters} />
+            )}
+
+            <Button
+                type="button"
+                color="btn-blue"
+                full
+                label="편지 남기기"
+                handleClick={() => {
+                    console.log("Button clicked");
+                }}
+            />
+            <div className="min-w-[100px] pt-[24px] text-center">
+                <button className="font-bold text-lg" onClick={() => {}}>
+                    내 편지함으로 돌아가기
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default LetterBox;
--- src/app/letterbox/_components/LetterModal.tsx
+++ src/app/letterbox/_components/LetterModal.tsx
@@ -1,14 +1,25 @@
 import { Button } from "@/components/common";
 import React from "react";
 
+type LetterType = {
+    id: string;
+    sender_id: string;
+    recipient_id: string;
+    content: string;
+    created_at: string;
+    envelope_type: number;
+    paper_type: number;
+    is_public: boolean;
+};
+
 type ModalProps = {
     isOpen: boolean;
     onClose: () => void;
-    senderId: string; // 클릭한 카드의 sender_id를 전달받음
+    contents: LetterType | null;
 };
 
-const LetterModal = ({ isOpen, onClose, senderId }: ModalProps) => {
-    if (!isOpen) return null; // 모달이 열려 있지 않으면 아무것도 렌더링하지 않음
+const LetterModal = ({ isOpen, onClose, contents }: ModalProps) => {
+    if (!isOpen || !contents) return null;
 
     return (
         <div
@@ -16,8 +27,8 @@ const LetterModal = ({ isOpen, onClose, senderId }: ModalProps) => {
             className=" backdrop-blur-sm fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center flex-col  z-50"
         >
             <div className="w-[600px] h-[640px]  bg-white p-6 ">
-                <h2>Sender: {senderId}</h2>
-                <p>모달 내용이 여기에 표시됩니다.</p>
+                <h2>Sender: {contents.sender_id}</h2>
+                <p>{contents.content}</p>
             </div>
             <div className="w-[600px] flex justify-between pt-[21px]">
                 <Button type="button" color="btn-red" label="삭제" full={false} handleClick={onClose} />
