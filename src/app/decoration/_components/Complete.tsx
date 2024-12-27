d5f68a438bb5d3e0fca956b24f1ef2b47b9a4af1
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-27 16:34:48 +0900

    fix : 편지함 가기 버튼 link 수정

--- src/app/decoration/_components/Complete.tsx
+++ src/app/decoration/_components/Complete.tsx
@@ -1,10 +1,20 @@
 "use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/common";
 import snake from "@/assets/images/snake.svg";
 
 const Complete = () => {
+    const [uid, setUid] = useState<string | null>(null);
+
+    useEffect(() => {
+        const url = new URL(window.location.href);
+        const pathSegments = url.pathname.split("/");
+        const extractedUid = pathSegments[pathSegments.length - 1];
+        setUid(extractedUid);
+    }, []);
+
     return (
         <section className="w-full h-full flex flex-col justify-between">
             <main className="flex flex-col justify-between h-[520px] place-items-center">
@@ -17,7 +27,7 @@ const Complete = () => {
                 <Image width={150} height={150} src={snake} alt="snake" />
             </main>
 
-            <Link href="/">
+            <Link href={`/letterbox/${uid}`}>
                 <Button type="button" color="btn-blue" full={true} label="편지함 가기" />
             </Link>
         </section>
